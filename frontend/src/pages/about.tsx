c5d2096bb92106ebcd5733cdc127ff57b610b29c
Iftekhar Riyad <iftekharriyad@protonmail.com>
2022-09-11 13:25:35 +0600

    created a confirmaton modal

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable canonical/filename-match-exported */
+import { type NextPage } from 'next';
+
+const About: NextPage = () => {
+  return <h1>About</h1>;
+};
+
+export default About;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,29 @@
 /* eslint-disable canonical/filename-match-exported */
+import React from 'react';
 import { type NextPage } from 'next';
+import { render } from 'react-dom';
+import { Modal } from '../components/Modal';
+import { useModal } from '../hooks/useModal';
+import { ConfirmationModal } from '../components/confirmation-modal'
 
 const Index: NextPage = () => {
-  return <h1>Welcome to Contra!</h1>;
+  const {isShown, toggle} = useModal()
+  const onConfirm = () => toggle();
+  const onCancel = () => toggle();
+
+  return (
+    <>
+      <button onClick={toggle}>Open Modal</button>
+      <Modal isShown={isShown} hide={toggle} 
+      modalContent={<ConfirmationModal
+                      onConfirm={onConfirm}
+                      onCancel={onCancel}
+                      message="Are you sure you want to delete element?"
+                      />
+      }
+      headerText="Important!"/>
+    </>
+  )
 };
 
 export default Index;
